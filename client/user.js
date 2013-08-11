413bccef19d8f792c6698caa53f5792308fa1c47
David Whittaker <dpwhittaker@gmail.com>
2013-08-11 18:13:25 -0400

    completed initial account system.

--- client/user.js
+++ client/user.js
@@ -4,7 +4,9 @@ Template.signUp.rendered = function() {
     valueKey: "SchoolName",
     local: Schools.find().fetch(),
     template: Template.schoolTypeahead
-  });
+  }).on('typeahead:selected typeahead:autocompleted', function(obj, datum) {
+    Template.signUp.selectedSchool = datum._id;
+  })  ;
   $(this.find('.twitter-typeahead')).css({display: "block"});
 };
 
@@ -23,17 +25,35 @@ Template.signUp.events({
   },
   'blur #email': function(event) {
     var target = $(event.target);
-    if (!/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(target.val()))
+    if (!/^[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(target.val()))
       target.parents(".validate-item").addClass("has-error");
   },
   'blur #password': function(event) {
     var target = $(event.target);
     if (!/^[a-zA-Z0-9`~!@#$%^&*()_+\[\]{}\\|;:'"\/?.,><]+$/.test(target.val()) ||
-      !/[a-zA-Z]/.test(target.val()) || !/[0-9]/.test(target.val()))
+      !/[a-zA-Z]/.test(target.val()) || !/[0-9]/.test(target.val()) || target.val().length < 8)
       target.parents(".validate-item").addClass("has-error");
   },
   'blur #confirmpass': function(event) {
     if ($('#confirmpass').val() !== $('#password').val())
       $('#confirmpass').parents(".validate-item").addClass("has-error");
+  },
+  'click #signUp, submit form': function(event) {
+    Accounts.createUser({
+      username: $('#userName').val(),
+      email: $('#email').val(),
+      password: $('#password').val(),
+      profile: {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        school: Template.signUp.selectedSchool,
+        userType: $('#userType').val()
+      }
+    }, function(err) {
+      if (err)
+        $(#errorMessage).html(err);
+      else
+        Meteor.router.to('/');
+    });
   }
 });
